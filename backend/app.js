1ce8f953de974101d82fdf63c4ab2588d3b2b2da
josepholayanju <joseph@mainstack.app>
2024-06-14 13:28:12 +0100

    fix: error in build

--- backend/app.js
+++ backend/app.js
@@ -50,20 +50,21 @@ if (process.env.NODE_ENV === "development") {
   mongoose.connect(process.env.MONGODB_PROD_URI, { useNewUrlParser: true });
   console.log("production db connected");
 }
-
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+  });
+  
 // port listen
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
 
 module.exports = app;
