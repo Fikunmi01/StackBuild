121bcdd6d0405b8f90f0315ff50d47074d7dde1b
josepholayanju <joseph@mainstack.app>
2024-06-20 00:03:13 +0100

    fix user update

--- backend/routes/user/user.routes.js
+++ backend/routes/user/user.routes.js
@@ -4,7 +4,7 @@ const router = Router();
 const { auth } = require("../../middleware/auth");
 
 router.post("/:userId/picture", uploadDP);
-router.patch("/update", auth, updateUser);
+router.patch("/:userId/update", auth, updateUser);
 router.get("/profile/:userId", auth, profile);
 
 module.exports = router;
