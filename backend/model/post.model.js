3aaa5e24ed4dbf86d433a8cdd62fa4bc967c0d00
Olayanju-1234 <horlarhyanjuhjoseph@gmail.com>
2024-08-19 10:50:07 +0100

    hotfix post model

--- backend/model/post.model.js
+++ backend/model/post.model.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 
 const postSchema = mongoose.Schema({
-    postId: { type: String, unique: true },
     title: String,
     content: String,
     imgSrc: String,
@@ -26,29 +25,6 @@ const postSchema = mongoose.Schema({
         default: new Date(),
     }
 })
-
-postSchema.pre('save', async function (next) {
-    const doc = this;
-
-    try {
-        if (!doc.isNew) {
-            // Document is being updated, no need to change postId
-            return next();
-        }
-
-        const lastDoc = await mongoose.model('Post', postSchema)
-            .findOne({}, {}, { sort: { postId: -1 } })
-            .exec();
-
-        // Set the id for the new document
-        const nextPostId = lastDoc ? parseInt(lastDoc.postId) + 1 : 1; // Convert to number and increment
-        doc.postId = nextPostId.toString(); // Convert back to string
-        next(); // Proceed to save the document
-    } catch (err) {
-        return next(err); // Handle any errors
-    }
-});
-
 
 const PostModel = mongoose.model('Post', postSchema)
 
